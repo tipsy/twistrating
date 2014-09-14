ed65b600c727a1f1d32333989c3e278e4ac095db
tipsy <davidaase@hotmail.com>
2014-09-14 09:56:02 +0200

    Made JSLinet happy.

--- js/main.js
+++ js/main.js
@@ -1,99 +1,97 @@
-$(function() {
+/*global $:false, Handlebars:false, console:false, FB: false, alert: false */
+$(function () {
+    'use strict';
+    var twistOverviewTemplate = Handlebars.compile($("#twist-overview-template").html()),
+        twistListTemplate     = Handlebars.compile($("#twist-list-template").html());
     
-    createFacebookShareButton();
-    createCopyLinkButton();
-    var twistOverviewTemplate = Handlebars.compile( $("#twist-overview-template").html() );
-    var twistListTemplate = Handlebars.compile( $("#twist-list-template").html() );
-
-    $.getJSON( "http://twistrating.apiary-mock.com/twists", function(data) {
-        createList(data);
-        createOverview(data);
-    })
-    .done(function() {
-        console.log("Lastet ned twist fra APIet");
-    })
-    .fail(function() {
-        alert( "Fant ingen twist i APIet... :/" );
-    });
-
-    function createList(twistData) {
-        var order = $(location).attr('href').split("#")[1] || "fail";
-        sortedTwists = sortTwists(twistData, order);
-        if(sortedTwists.twists.length === twistData.twists.length) {
-            twistData = sortedTwists;
-            goToOrderPage();
-        }else{
-            goToOverviewPage();
-        }
-
-        $("#twist-list-template-output").html(twistListTemplate(twistData));
-        $("#sortable-twist-list").sortable();
+    function sortTwists(twistData, order) {
+        var twists = twistData.twists,
+            sortedTwists = [];
+        $(order.split("")).each(function (i) {
+            var charId = this[0],
+                foundTwists = twists.filter(function (twist) {
+                    return (twist.charId === charId);
+                });
+            sortedTwists.push(foundTwists[0]);
+        });
+        return { twists : sortedTwists };
     }
-
+    
     function goToOrderPage() {
-        $("#rate,#rate-tab").addClass("active in"); 
+        $("#rate,#rate-tab").addClass("active in");
     }
 
     function goToOverviewPage() {
         $("#overview,#overview-tab").addClass("active in");
     }
 
-    function sortTwists(twistData, order) {
-        twists = twistData.twists;
-        var sortedTwists = [];
-        var usedCharIds = [];
-        for (var i = 0; i < order.length; i++) {
-            var charId = order.charAt(i);
-            var foundTwists = twists.filter(function (twist) {
-                return (twist.charId === charId);
-            });
-            if (foundTwists.length === 1 && usedCharIds.indexOf(charId) === -1) {
-                sortedTwists.push(foundTwists[0]);
-                usedCharIds.push(charId);
-            }
+    function createList(twistData) {
+        var order = $(location).attr('href').split("#")[1] || "fail",
+            sortedTwists = sortTwists(twistData, order);
+        if (sortedTwists.twists.length === twistData.twists.length) {
+            twistData = sortedTwists;
+            goToOrderPage();
+        } else {
+            goToOverviewPage();
         }
-        return { twists : sortedTwists };
+        $("#twist-list-template-output").html(twistListTemplate(twistData));
+        $("#sortable-twist-list").sortable();
     }
 
     function createOverview(twistData) {
         $("#twist-overview-template-output").html(twistOverviewTemplate(twistData));
         $(".twist-wrapper").last().css("margin-bottom", "70px"); //hack, not sure why needed...
-        $(".btn-twist").click(function() {
-            var id = $(this).data("id");
-            var rating = $(this).data("value");
+        $(".btn-twist").click(function () {
+            var id = $(this).data("id"),
+                rating = $(this).data("value");
             $.ajax({
-              url: "http://twistrating.apiary-mock.com/twists",
-              type: "POST",
-              data: JSON.stringify({ "id" : id, "rating" : rating } ),
-              contentType:"application/json; charset=utf-8",
-              dataType:"json",
+                url: "http://twistrating.apiary-mock.com/twists",
+                type: "POST",
+                data: JSON.stringify({ "id" : id, "rating" : rating }),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json"
             });
         });
     }
-
-    function createCopyLinkButton(){
-        $(".copy-link").click(function(){
-            $(".link-container").text("http://twistrating.no#"+getTwistOrder());
-            $(".link-container").attr("href", "http://twistrating.no#"+getTwistOrder());
-        });
-    }
-
-    function createFacebookShareButton() {
-        $(".share-on-facebook").click(function(){
-            console.log("TwistRating: " + getTwistOrder());
-            FB.ui({
-                method: 'share',
-                href: 'http://twistrating.no#'+getTwistOrder(),
-            }, function(response){});
-        });
-    }
-
-    function getTwistOrder(){
+    
+    function getTwistOrder() {
         var order = "";
-        $("#sortable-twist-list li").each(function(){
+        $("#sortable-twist-list li").each(function () {
             order += $(this).data("charid");
         });
         return order;
     }
+
+    function createCopyLinkButton() {
+        $(".copy-link").click(function () {
+            $(".link-container").text("http://twistrating.no#" + getTwistOrder());
+            $(".link-container").attr("href", "http://twistrating.no#" + getTwistOrder());
+        });
+    }
+
+    function createFacebookShareButton() {
+        $(".share-on-facebook").click(function () {
+            console.log("TwistRating: " + getTwistOrder());
+            FB.ui({
+                method: 'share',
+                href: 'http://twistrating.no#' + getTwistOrder()
+            }, function (response) {});
+        });
+    }
+    
+    function downloadTwistsAndBuildSite() {
+        $.getJSON("http://twistrating.apiary-mock.com/twists", function (data) {
+            createList(data);
+            createOverview(data);
+        }).done(function () {
+            console.log("Lastet ned twist fra APIet");
+        }).fail(function () {
+            alert("Fant ingen twist i APIet... :/");
+        });
+    }
+    
+    createFacebookShareButton();
+    createCopyLinkButton();
+    downloadTwistsAndBuildSite();
     
 });
